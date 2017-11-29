8fc2068cdd34a0d6ae9f65a487c0fcd563ca20e2
mnfmnfm <mnfmnfm@gmail.com>
2017-11-29 09:31:36 -0800

    add counter to login solution

--- solutions/login.js
+++ solutions/login.js
@@ -6,12 +6,20 @@ var passwordInput;
 
 var loggedIn = false;
 
-while (!loggedIn){
+let failures = 0;
+
+while (!loggedIn && failures < 3){
 	passwordInput = prompt("Enter password for user " + userLogin.userName + ".")
 	if (passwordInput === userLogin.password){
 		console.log("Passwords match!");
     loggedIn = true;
 	} else {
 		console.log("Passwords do not match. Try Again!");
+    failures += 1;
+    // failures = failures + 1;
+    // failures++;
 	}
+}
+if (failures === 3) {
+  alert("you failed");
 }
